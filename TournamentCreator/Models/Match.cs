2ab20e81db95b500f6c26a18b753268359512469
Oleg <Oleg58807@ukr.net>
2019-03-02 18:40:38 +0200

    Added Match and Team classes

---
+++ TournamentCreator/Models/Match.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TournamentCreator.Models
+{
+    public class Match
+    {
+        public Team HomeTeam { get; set; }
+        public Team AwayTeam { get; set; }
+        public int HomeScore { get; set; }
+        public int AwayScore { get; set; }
+    }
+}
---
+++ TournamentCreator/Models/Team.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TournamentCreator.Models
+{
+    public class Team
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Stadium { get; set; }
+    }
+}
