bc4b8477881ceee0349d6dfef3e0296e5aa33869
Oleg <Oleg58807@ukr.net>
2019-03-04 22:38:05 +0200

    Trying database connection

---
+++ TournamentCreator/Models/TeamContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TournamentCreator.Models
+{
+    public class TeamContext : DbContext
+    {
+        public TeamContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
